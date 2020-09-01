4074ad01ef538d71175d9b91730b485c0d107488
Maninder Singh <manindermatharu63@gmail.com>
2020-09-01 16:32:00 +0530

    Updates

---
+++ precache-manifest.471fe81cd4ed8ea4cf990106523c6c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2f31f0c9e8061f7e606",
+    "url": "/react-live/static/css/main.8ba177d7.chunk.css"
+  },
+  {
+    "revision": "d2f31f0c9e8061f7e606",
+    "url": "/react-live/static/js/main.8d778841.chunk.js"
+  },
+  {
+    "revision": "dbb9013f708edef95d90",
+    "url": "/react-live/static/js/runtime~main.9dbd6556.js"
+  },
+  {
+    "revision": "fcfb1b0132f811345909",
+    "url": "/react-live/static/js/2.6a6cb4c1.chunk.js"
+  },
+  {
+    "revision": "3b433ece35ad46b88047f86e201289a5",
+    "url": "/react-live/index.html"
+  }
+];
